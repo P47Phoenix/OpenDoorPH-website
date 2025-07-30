165da87efc2ea9000e3a802ed000343c46134d93
Michael Connelly <michaelconne@gmail.com>
2025-07-30 07:18:19 -0500

    fix: add accessibility titles to iframe elements
    - Add descriptive title to Google Maps iframe in Location.js
    - Add descriptive title to YouTube playlist iframe in Video.js
    - Resolves ESLint jsx-a11y/iframe-has-title warnings
    - Improves accessibility for screen readers and assistive technologies

--- OpenDoorWebsiteApp/src/Pages/Location.js
+++ OpenDoorWebsiteApp/src/Pages/Location.js
@@ -16,7 +16,7 @@ export const Location = () => {
             </div>
             <div class="map">
                 <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0"
-                        marginwidth="0"
+                        marginwidth="0" title="Google Maps location for Open Door Full Gospel Church"
                         src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=135+S+1st+St,+Pleasant+Hill,+Missouri+64080+&amp;sll=38.784773,-94.274362&amp;sspn=0.001372,0.002411&amp;ie=UTF8&amp;hq=&amp;hnear=135+S+1st+St,+Pleasant+Hill,+Cass,+Missouri+64080&amp;ll=38.792159,-94.269133&amp;spn=0.023414,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed">
                 </iframe>
                 <br/>
--- OpenDoorWebsiteApp/src/Pages/Video.js
+++ OpenDoorWebsiteApp/src/Pages/Video.js
@@ -4,7 +4,7 @@ export const Video = () => {
     return (
         <div>
             <iframe src="https://www.youtube.com/embed/videoseries?list=PLTEzkRjWlPyQhKteW1xKjNGv42h0-kzTr" width="480"
-                    height="400"></iframe>
+                    height="400" title="Open Door Full Gospel Church YouTube playlist"></iframe>
         </div>
     )
 }
