6ff5ddc6e354e359e3aef95675f72973112e0a80
Michael Connelly <michaelconne@gmail.com>
2025-08-02 19:21:39 -0500

    feat: generate core SVG asset library with church-themed designs
    - Create 7 new SVG assets to replace legacy GIF images
    - Add church color palette (#D4C4A0, #C8B59B, #9EC630) to all SVGs
    - Include clock, comment, page, table decorations, header, and background
    - Add TypeScript declarations for SVG modules
    - Update asset exports in index files
    - All assets are scalable and responsive-ready

---
+++ OpenDoorWebsiteApp/src/assets/svg/index.ts
@@ -0,0 +1,10 @@
+// SVG Asset Exports
+// Church-themed SVG icons and graphics for Open Door Baptist Church website
+
+export { default as ClockIcon } from './clock-icon.svg';
+export { default as CommentIcon } from './comment-icon.svg';
+export { default as PageIcon } from './page-icon.svg';
+export { default as TableLeft } from './table-left.svg';
+export { default as TableRight } from './table-right.svg';
+export { default as HeaderBackground } from './header-bg.svg';
+export { default as BackgroundPattern } from './background.svg';
