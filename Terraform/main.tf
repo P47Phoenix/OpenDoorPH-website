dfec376bcc62a321e78678171d82061efd9331ea
Michael Connelly <michaelconne@gmail.com>
2026-04-05 10:34:03 -0500

    workflow for terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,6 +7,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "Some comment"
 }
@@ -73,7 +78,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -92,6 +97,54 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+# -------------------------------------------------------------------
+# ACM Certificate (must be in us-east-1 for CloudFront)
+# -------------------------------------------------------------------
+
+resource "aws_acm_certificate" "cert" {
+  provider          = aws.us_east_1
+  domain_name       = "opendoorph.info"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "opendoorph.net",
+    "opendoorph.org",
+    "opendoorph.com",
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# -------------------------------------------------------------------
+# ACM Certificate Validation
+# Apply this AFTER adding the DNS CNAME records shown in the outputs.
+# -------------------------------------------------------------------
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider        = aws.us_east_1
+  certificate_arn = aws_acm_certificate.cert.arn
+}
+
+# -------------------------------------------------------------------
+# Outputs – DNS validation CNAME records to add manually
+# -------------------------------------------------------------------
+
+output "acm_validation_records" {
+  description = "DNS CNAME records required to validate the ACM certificate. Add these to your DNS provider."
+  value = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
   }
 }
